92df30c8064abc8106897c04503366b3a6c19819
EricoBruner <ericodasilvabruner@gmail.com>
2023-09-13 11:12:51 -0300

    finalizado aula 08

---
+++ aula-ts-08/index.ts
@@ -0,0 +1,30 @@
+type Game = {
+  id: number;
+  platform: {
+    id: number;
+    name: string;
+  };
+  title: string;
+  publisher: string;
+  launch?: string | Date;
+};
+
+const game2: Game = {
+  id: 1,
+  platform: {
+    id: 1,
+    name: "Playstation",
+  },
+  title: "The Last of Us",
+  publisher: "Naughty Dog",
+  launch: "2013-06-14", // pode ser um Date. também pode ser vazio
+};
+
+const games2: Game[] = [game2];
+
+function play(game: Game) {
+  // runs the game
+  console.log(game);
+}
+
+play(games2[0]);
