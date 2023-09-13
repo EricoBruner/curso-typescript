5e7f264229ca237dd0e768bebefaa230be7c43e1
EricoBruner <ericodasilvabruner@gmail.com>
2023-09-13 10:59:55 -0300

    finalizado aula 07

---
+++ aula-ts-07/index.ts
@@ -0,0 +1,17 @@
+import { Express, Response, Request } from "express";
+import express from "express";
+
+const app: Express = express();
+
+app.get("/health", (req: Request, res: Response) =>
+  res.status(200).send("I'm Ok!")
+);
+
+app.get("/today", (req: Request, res: Response) => {
+  res.send({
+    today: new Date().toLocaleDateString("pt-br"),
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is up and running on port ${port}`));
